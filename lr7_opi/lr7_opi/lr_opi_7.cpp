e2da098bb7ff234306e7f3e04faeda7a3be252b4
Pich Tym <pichuhintymur@gmail.com>
2024-12-04 07:12:33 +0200

    Додано новий проєкт (lr_opi_7)

---
+++ lr7_opi/lr7_opi/lr_opi_7.cpp
@@ -0,0 +1,24 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);//Підтримка кирилиці
+
+	float y, x, h;
+	x = 2;//Початковий х з умови задачі
+	h = 0.2;//Крок збільшення х за 1 ітерацію циклу з умови задачі
+
+	do {//Цикл do while, потрібний за умовою
+		y = pow(sin(x), 2) + 2 * cos(x) + 3;//Функція
+		printf("Функція у = %f\n", y);//Вивід значення функції
+		x += h;//Лічильник
+
+	} while (x < 4 + h);//Умова циклу do while
+}
